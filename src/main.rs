bbfb83ce8984c35cd35d2ee64afd6074dbf82f04
Tao119 <tao.dama.art@gmail.com>
2024-04-23 10:41:36 +0900

    port setting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
+use std::env;
+
 #[actix_web::main]
 pub async fn main() -> std::io::Result<()> {
+    let port_key = "PORT";
+    let default_port = 8080;
+    let port = match env::var(port_key) {
+        Ok(val) => match val.parse::<u16>() {
+            Ok(port) => port,
+            Err(_) => {
+                println!(
+                    "the port number \"{}\" is invalid. default port will be used.",
+                    val
+                );
+                default_port
+            }
+        },
+        Err(_) => {
+            println!(
+                "\"{}\" is not defined in environment variables. default port will be used.",
+                port_key
+            );
+            default_port
+        }
+    };
     println!("launching server...");
-    techlinter_server::apis::create_app("0.0.0.0", 8080).await
+    techlinter_server::apis::create_app("0.0.0.0", port).await
 }
